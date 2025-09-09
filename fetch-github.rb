639bfd1815f091f8ce5af85ff8ab257293c61136
Jon Ericson <jon@jlericson.com>
2025-09-08 21:27:48 -0700

    Discussions and better caching

--- fetch-github.rb
+++ fetch-github.rb
@@ -8,10 +8,11 @@ require 'htmlentities'
 require 'date'
 require 'pg'
 require 'optparse'
+require 'fileutils'
 
 options = {
   :github_token => ENV['GITHUB_TOKEN'],
-  :query_limit => 120,
+  :query_limit => 2000,
   :cache_file => 'issues.csv'
 }
 
@@ -27,8 +28,16 @@ optparse = OptionParser.new do |opts|
     options[:query_limit] = s
   end
 
-  opts.on("-c", "--cache-file", "File to cache results") do |s|
+  opts.on("-c", "--cache-file FILE", "File to cache results") do |s|
     options[:cache_file] = s
+  end
+
+  opts.on("-j", "--json-dir DIRECTORY", "Directory for saving JSON versions of issues") do |s|
+    options[:json_dir] = s
+  end
+
+  opts.on("-u", "--updates", "Fetch the latest updates") do |s|
+    options[:updates] = s
   end
 
 end
@@ -54,12 +63,31 @@ end
 @user = ARGV.shift
 @repo = ARGV.shift
 
-@uri = URI("https://api.github.com/repos/#{@user}/#{@repo}/issues")
 
 @more = true
 @page = 1
 
+def insert_issue(issue, issues, options)
 
+  if options[:json_dir]
+    if FileUtils.mkdir_p(options[:json_dir])
+      File.write(File.join(options[:json_dir], "#{issue['number']}.json"),
+                 issue.to_json)
+    end
+  end
+  
+  issue['author'] = issue['user']['login']
+
+  if issue['html_url'] =~ %r"^https://github.com/[^/]+/[^/]+/([^/]+)/\d+$"
+    issue['type'] = $1
+  end
+
+  if issue['type'] == 'discussions'
+    issue['closed_at'] = ''
+  end
+
+  issues[issue['number']] = issue
+end
 
 def fetch_issue(uri, issues, options)
   @response = HTTParty.get(uri,
@@ -71,13 +99,16 @@ def fetch_issue(uri, issues, options)
   when 200
     body= JSON.parse(@response.body)
     if body.kind_of?(Array)
-      issue = body.first
+      body.each do |issue|
+        insert_issue(issue, issues, options)
+      end
     else
       issue = body
+    
+      insert_issue(issue, issues, options)
     end
-    issue['author'] = issue['user']['login']
-    issues[issue['number']] = issue
-  when 403
+  when 404
+    return 404
   else
     STDERR.puts @uri, @response.body, @response.code, @response.message, @response.headers.inspect
   end
@@ -86,38 +117,68 @@ def fetch_issue(uri, issues, options)
   #STDERR.puts @response.headers['x-ratelimit-limit']
   #STDERR.puts @response.headers['link']
 
-  return @response.headers['x-ratelimit-limit']
+  return @response
   
 end
 
-@uri.query = URI.encode_www_form({
-                                   :per_page => 1,
-                                   :sort => 'created',
-                                   :direction => 'desc'
-                                 })
+@uri = URI("https://api.github.com/repos/#{@user}/#{@repo}/issues")
 
+while @more
+  @uri.query = URI.encode_www_form({
+                                     :per_page => 100,
+                                     :page => @page,
+                                     :sort => 'updated',
+                                     :direction => 'desc'
+                                   })
 
+  ret = fetch_issue(@uri, issues, options)
 
-fetch_issue(@uri, issues, options)
+  STDERR.puts ret.headers['x-ratelimit-limit']
+  STDERR.puts ret.headers['link']
+  @more = /rel="next"/.match?ret.headers['link']
+  @page += 1
 
+  @more = false unless options[:updates]
+  @more = false if @page > options[:query_limit]
+end
+
+#for i in 21355..issues.keys.max # Test first discussion
 for i in 1..issues.keys.max
   break if @page > options[:query_limit]
   next if issues[i];
   @page += 1
+
+  if options[:json_dir]
+    path = File.join(options[:json_dir], "#{i}.json")
+    if File.file? path
+      file = File.open path
+      issue = JSON.load file
+      insert_issue(issue, issues, options)
+      next
+    end
+  end
   
   @uri = URI("https://api.github.com/repos/#{@user}/#{@repo}/issues/#{i}")
 
   ret=fetch_issue(@uri, issues, options)
-  break if ret.to_i < 100
+
+  if ret == 404
+    @discussion_uri = URI("https://api.github.com/repos/#{@user}/#{@repo}/discussions/#{i}")
+    ret = fetch_issue(@discussion_uri, issues, options)
+  end
+
+  next if ret == 404
+  
+  break if ret.headers['x-ratelimit-limit'].to_i < 100
   
 end
 
-fields = ['number', 'title', 'author', 'state', 'created_at', 'closed_at', 'updated_at', 'author_association', 'comments']
+fields = ['number', 'title', 'type', 'author', 'state', 'created_at', 'closed_at', 'updated_at', 'author_association', 'comments']
 
 File.write(options[:cache_file], fields.to_csv)
 
 issues.sort.to_h.values.each do |i|
-  row =  i.slice('number', 'title', 'author', 'state', 'created_at', 'closed_at', 'updated_at', 'author_association', 'comments').values.to_csv
+  row =  i.slice('number', 'title', 'type', 'author', 'state', 'created_at', 'closed_at', 'updated_at', 'author_association', 'comments').values.to_csv
   File.write(options[:cache_file], row, mode: 'a')
 end
 
